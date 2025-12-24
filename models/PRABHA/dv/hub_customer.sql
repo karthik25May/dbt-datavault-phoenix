669d1c2099d138407be64e410df719705f3e2e08
Prabha Ganesan <2023ac05414@wilp.bits-pilani.ac.in>
2025-12-24 06:45:59 +0000

    created hub and satellite

--- models/PRABHA/dv/hub_customer.sql
+++ models/PRABHA/dv/hub_customer.sql
@@ -1,3 +1,7 @@
 {{ automate_dv.hub(
-    source_model='v_stg_cust'
+    source_model='v_stg_cust',
+    src_nk='C_CUSTKEY',
+    src_pk='CUSTOMER_HK',
+    src_ldts='LOAD_DATETIME',
+    src_source='RECORD_SOURCE'
 ) }}
--- models/PRABHA/dv/satellite_customer.sql
+++ models/PRABHA/dv/satellite_customer.sql
@@ -1,3 +1,16 @@
 {{ automate_dv.sat(
-    source_model='v_stg_cust'
+    source_model='v_stg_cust',
+    src_pk='CUSTOMER_HK',
+    src_hashdiff='CUSTOMER_HASHDIFF',
+     src_payload=[
+        'C_NAME',
+        'C_ADDRESS',
+        'C_NATIONKEY',
+        'C_PHONE',
+        'C_ACCTBAL',
+        'C_MKTSEGMENT',
+        'C_COMMENT'
+    ],
+    src_ldts='LOAD_DATETIME',
+    src_source='RECORD_SOURCE'
 ) }}
