392681da1aa227d37f99171602a16665b9c5bfc4
Prabha Ganesan <2023ac05414@wilp.bits-pilani.ac.in>
2025-12-30 10:29:41 +0000

    Test

--- models/PRABHA/raw/raw_line_item.sql
+++ models/PRABHA/raw/raw_line_item.sql
@@ -2,6 +2,9 @@
 unique_key=['L_ORDERKEY', 'L_LINENUMBER'] 
 )
 }}
-select *
+select * , CURRENT_TIMESTAMP() AS LOAD_DATETIME
 from {{ source('tpch', 'LINEITEM') }}
-LIMIT 10000
+{% if is_incremental() %}
+WHERE (L_ORDERKEY,L_LINENUMBER) NOT IN (SELECT L_ORDERKEY,L_LINENUMBER from DBT_PRABHA.RAW_PRABHA.raw_line_item )
+{% endif %}
+LIMIT 200
